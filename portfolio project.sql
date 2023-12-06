f8abf2c4f3e7dec630b21918f114fe677fa1ab23
ByronOdera <146132653+ByronOdera@users.noreply.github.com>
2023-12-06 02:54:17 -0800

    Update portfolio project.sql

--- portfolio project.sql
+++ portfolio project.sql
@@ -131,3 +131,59 @@ JOIN PortfolioProject..CovidVaccinations vac
 	ON dea.location = vac.location
 	AND dea.date = vac.date
 WHERE dea.continent IS NOT NULL
+
+-- TABLEAU TABLE 1
+
+SELECT SUM(new_cases) AS total_cases, SUM(CONVERT(float,new_deaths)) AS total_deaths , SUM(CONVERT(float,new_deaths))/SUM(new_cases) * 100 AS DeathPercentage
+FROM PortfolioProject..CovidDeaths
+WHERE location = 'World'
+ORDER BY 1,2
+
+-- TABLE 2
+
+SELECT location,SUM(CONVERT(float,new_deaths)) AS TotalDeathCount
+FROM PortfolioProject..CovidDeaths
+WHERE continent IS NULL
+AND location NOT IN ('World','European Union','International','High Income','Upper middle income','Lower middle income','Low income')
+GROUP BY location
+ORDER BY TotalDeathCount DESC
+
+-- TABLE 3
+
+SELECT location,population,MAX(total_cases) AS HighestInfectionCount, MAX(total_cases)/population*100 AS PopulationPercentInfected
+FROM PortfolioProject..CovidDeaths
+GROUP BY location,population
+ORDER BY PopulationPercentInfected DESC
+
+
+-- TABLE 4
+
+
+SELECT location,population,date,MAX(total_cases) AS HighestInfectionCount, MAX(total_cases)/population*100 AS PopulationPercentInfected
+FROM PortfolioProject..CovidDeaths
+GROUP BY location,population,date
+ORDER BY PopulationPercentInfected DESC
+
+-- TABLE 5
+
+WITH Pop_vs_Vac (continent,location,date,population,new_vaccinations,RollingPeopleVaccinated)
+AS
+(
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(FLOAT,vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location,dea.date) AS RollingPeopleVaccinated
+FROM PortfolioProject..CovidDeaths dea
+JOIN PortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+)
+SELECT *, (RollingPeopleVaccinated/population)*100
+	FROM Pop_vs_Vac
+
+
+-- TABLE 6
+
+SELECT location,date,population,total_cases,total_deaths
+FROM PortfolioProject..CovidDeaths
+WHERE continent IS NOT NULL
+ORDER BY 1,2
